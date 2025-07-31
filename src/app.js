113fcda7ca23a7fcbc84c0dbfbcf7058e4b9e439
Tribikram Sen <sentribikram@gmail.com>
2025-07-31 21:56:11 +0545

    handle cookie and cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const app = express()
-const helmet = require('helmet')
+const express = require('express');
+const app = express();
+const cookieParser = require("cookie-parser");
+const helmet = require('helmet');
 const cors = require('cors')
 const logger = require('./config/winstonLoggerConfig')
-const errorHandler = require('./utils/errorHandler')
-const { NODE_ENV } = require('./config/constant')
+const errorHandler = require('./utils/errorHandler');
 const { authRoutes } = require('./routes')
 
-app.use(cors())
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // if you're sending cookies or auth headers
+};
+
+app.use(cors(corsOptions));
 app.use(helmet())
+app.use(cookieParser());
 app.use(express.json())
 
 require('./config/database')
