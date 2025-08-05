0c9e7ac3cdc76f2d0fa0e2e90277cf42d8c28577
Tribikram Sen <sentribikram@gmail.com>
2025-08-05 18:11:46 +0545

    category create update and pagination

--- src/controllers/master/category.controller.js
+++ src/controllers/master/category.controller.js
@@ -1,16 +1,26 @@
-const {categoryService} = require("../../services/master");
-
+const { categoryService } = require("../../services/master");
 const logger = require("../../config/winstonLoggerConfig");
-const { getFormattedDate } = require("../../helpers/date");
-const Nepali_Calendar = require("../../helpers/nepaliCalendar");
 const { DATA_SAVED, SUCCESS_API_FETCH } = require("../../helpers/response");
+
+const { UniqueConstraintError } = require("sequelize");
 
 const createCategory = async (req, res, next) => {
   try {
     const value = req.body;
-
+    const isCategoryNameAvailable = await categoryService.checkCategoryNameAvailable(
+      value.name);
+    if (!isCategoryNameAvailable) {
+      return res.status(208).json({
+        status: false,
+        message: "Category name must be unique"
+      });
+    }
     const category = await categoryService.createCategory(value);
-    res.status(201).json(DATA_SAVED(category));
+    res.status(201).json({
+      status: true,
+      message: "Category created successfully",
+      data: category
+    });
   } catch (error) {
     logger.error(
       `{ Api:${req.url}, Error:${error.message}, stack:${error.stack} }`
@@ -19,9 +29,55 @@ const createCategory = async (req, res, next) => {
   }
 };
 
+const updateCategory = async (req, res, next) => {
+  try {
+
+    const value = req.body;
+    const checkData = await categoryService.getCategoryById(req.params.id);
+
+    if (!checkData) {
+      return res.status(404).json({
+        status: false,
+        message: "Category not found!!!"
+      });
+    }
+
+    const isCategoryNameAvailable = await categoryService.checkCategoryNameAvailable(
+      value.name, req.params.id);
+
+    if (!isCategoryNameAvailable) {
+      return res.status(208).json({
+        status: false,
+        message: "Category name must be unique"
+      });
+    }
+
+    const updatedCategory = await categoryService.updateCategory(
+      req.params.id,
+      value
+    );
+
+    return res.status(200).json({
+      status: true,
+      message: "Category updated successfully",
+      data: updatedCategory
+    });
+  } catch (error) {
+    if (error instanceof UniqueConstraintError) {
+      return res.status(400).json({ error: "Category name must be unique" });
+    }
+    logger.error(
+      `{ Api:${req.url}, Error:${error.message}, stack:${error.stack} }`
+    );
+    return next(error);
+  }
+};
+
+
 const getAllCategories = async (req, res, next) => {
   try {
-    const categories = await categoryService.getAllCategories(req.query.status);
+    const { page, limit, status } = req.query;
+    const categories = await categoryService.getAllCategories(page, limit, status);
     res.json(categories);
   } catch (error) {
     logger.error(
@@ -44,24 +100,7 @@ const getCategoryById = async (req, res, next) => {
   }
 };
 
-const updateCategory = async (req, res, next) => {
-  try {
-    const value = req.body;
 
-    const updatedCategory = await categoryService.updateCategory(
-      req.params.id,
-      value
-    );
-    if (!updatedCategory)
-      return res.status(404).json({ error: "Category not found" });
-    res.json(updatedCategory);
-  } catch (error) {
-    logger.error(
-      `{ Api:${req.url}, Error:${error.message}, stack:${error.stack} }`
-    );
-    return next(error);
-  }
-};
 
 const deleteCategory = async (req, res, next) => {
   try {
