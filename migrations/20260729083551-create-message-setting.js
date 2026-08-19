5030ec278009215deedf1d71dae0ccf02ec7118a
sachinkharel444 <ukharel111@gmail.com>
2026-08-19 14:26:42 +0545

    migration file

---
+++ migrations/20260729083551-create-message-setting.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('message_setting', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.STRING, // or Sequelize.TEXT if messages can be long
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.TINYINT,
+        defaultValue: 1,
+      },
+      created_by: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('message_setting');
+  }
+};
